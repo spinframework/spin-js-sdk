a08eadc1351f11c0f84f9a03eec70568461a90ce
Joel Dice <joel.dice@fermyon.com>
2022-08-23 16:37:56 -0600

    add outbound HTTP support
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- crates/spin-js-engine/src/lib.rs
+++ crates/spin-js-engine/src/lib.rs
@@ -1,7 +1,7 @@
 use {
-    anyhow::Result,
+    anyhow::{Error, Result},
     bytes::Bytes,
-    http::{header::HeaderName, HeaderValue},
+    http::{header::HeaderName, request, HeaderValue},
     once_cell::sync::OnceCell,
     quickjs_wasm_rs::{Context, Deserializer, Serializer, Value},
     quickjs_wasm_sys::{
@@ -11,9 +11,10 @@ use {
     spin_sdk::{
         config,
         http::{Request, Response},
-        http_component,
+        http_component, outbound_http,
     },
     std::{
+        borrow::Cow,
         collections::BTreeMap,
         io::{self, Read},
         ops::Deref,
@@ -25,6 +26,35 @@ use {
 static mut CONTEXT: OnceCell<Context> = OnceCell::new();
 static mut GLOBAL: OnceCell<Value> = OnceCell::new();
 static mut ENTRYPOINT: OnceCell<Value> = OnceCell::new();
+
+#[derive(Serialize)]
+struct InboundRequest<'a> {
+    headers: BTreeMap<&'a str, &'a [u8]>,
+    body: Option<&'a [u8]>,
+}
+
+#[derive(Deserialize)]
+struct InboundResponse {
+    status: u16,
+    headers: BTreeMap<Box<str>, Box<str>>, // TODO: use Box<[u8]> for values
+    body: Option<Box<str>>,                // TODO: use Box<[u8]>
+}
+
+#[derive(Deserialize)]
+struct OutboundRequest {
+    method: Box<str>,
+    uri: Box<str>,
+    #[serde(default)]
+    headers: BTreeMap<Box<str>, Box<str>>, // TODO: use Box<[u8]> for values
+    body: Option<Box<str>>, // TODO: use Box<[u8]>
+}
+
+#[derive(Serialize)]
+struct OutboundResponse<'a> {
+    status: u16,
+    headers: BTreeMap<&'a str, Cow<'a, str>>, // TODO: use &'a [u8] for values
+    body: Option<Cow<'a, str>>,               // TODO: use &'a [u8]
+}
 
 unsafe fn spin_get_config(
     ctx: *mut JSContext,
@@ -44,15 +74,79 @@ unsafe fn spin_get_config(
             key_ptr as *const u8,
             key_length as usize,
         ))
-        .ok()
-        .and_then(|key| config::get(key).ok());
+        .map_err(Error::from)
+        .and_then(|key| config::get(key).map_err(Error::from));
 
         JS_FreeCString(ctx, key_ptr);
 
-        if let Some(value) = value {
-            JS_NewStringLen(ctx, value.as_ptr() as _, value.len() as _)
-        } else {
-            ext_js_exception
+        match value {
+            Ok(value) => JS_NewStringLen(ctx, value.as_ptr() as _, value.len() as _),
+            Err(e) => {
+                eprintln!("{e:?}");
+                ext_js_exception
+            }
+        }
+    } else {
+        ext_js_exception
+    }
+}
+
+fn send_http_request(context: &Context, request: Value) -> Result<Value> {
+    let request = OutboundRequest::deserialize(&mut Deserializer::from(request))?;
+
+    let mut builder = request::Builder::new()
+        .method(request.method.deref())
+        .uri(request.uri.deref());
+
+    if let Some(headers) = builder.headers_mut() {
+        for (key, value) in &request.headers {
+            headers.insert(
+                HeaderName::from_bytes(key.as_bytes())?,
+                HeaderValue::from_str(value.deref())?,
+            );
+        }
+    }
+
+    let response = outbound_http::send_request(
+        builder.body(
+            request
+                .body
+                .map(|bytes| Bytes::copy_from_slice(bytes.as_bytes())),
+        )?,
+    )?;
+
+    let response = OutboundResponse {
+        status: response.status().as_u16(),
+        headers: response
+            .headers()
+            .iter()
+            .map(|(key, value)| (key.as_str(), String::from_utf8_lossy(value.as_bytes())))
+            .collect(),
+        body: response
+            .body()
+            .as_ref()
+            .map(|bytes| String::from_utf8_lossy(bytes.deref())),
+    };
+
+    let mut serializer = Serializer::from_context(context)?;
+    response.serialize(&mut serializer)?;
+    Ok(serializer.value)
+}
+
+unsafe fn spin_send_http_request(
+    ctx: *mut JSContext,
+    _this: JSValue,
+    argc: c_int,
+    argv: *mut JSValue,
+    _magic: c_int,
+) -> JSValue {
+    if argc == 1 {
+        match send_http_request(&Context::from_raw(ctx), Value::from_raw(ctx, *argv)) {
+            Ok(response) => response.to_raw(),
+            Err(e) => {
+                eprintln!("{e:?}");
+                ext_js_exception
+            }
         }
     } else {
         ext_js_exception
@@ -84,8 +178,20 @@ pub extern "C" fn init() {
             .unwrap(),
         )
         .unwrap();
+    let http = context.object_value().unwrap();
+    http.set_property(
+        "send",
+        unsafe {
+            context.new_callback(|ctx, this, argc, argv, magic| {
+                spin_send_http_request(ctx, this, argc, argv, magic)
+            })
+        }
+        .unwrap(),
+    )
+    .unwrap();
     let spin = context.object_value().unwrap();
     spin.set_property("config", config).unwrap();
+    spin.set_property("http", http).unwrap();
     global.set_property("spin", spin).unwrap();
 
     unsafe {
@@ -93,19 +199,6 @@ pub extern "C" fn init() {
         GLOBAL.set(global).unwrap();
         ENTRYPOINT.set(entrypoint).unwrap();
     }
-}
-
-#[derive(Serialize)]
-struct MyRequest<'a> {
-    headers: BTreeMap<&'a str, &'a [u8]>,
-    body: Option<&'a [u8]>,
-}
-
-#[derive(Deserialize)]
-struct MyResponse {
-    status: u16,
-    headers: BTreeMap<Box<str>, Box<str>>,
-    body: Option<Box<str>>,
 }
 
 #[http_component]
@@ -120,7 +213,7 @@ fn handle(request: Request) -> Result<Response> {
         entrypoint = ENTRYPOINT.get().unwrap();
     }
 
-    let request = MyRequest {
+    let request = InboundRequest {
         headers: request
             .headers()
             .iter()
@@ -135,8 +228,7 @@ fn handle(request: Request) -> Result<Response> {
 
     let response = entrypoint.call(global, &[request])?;
 
-    let mut deserializer = Deserializer::from(response);
-    let response = MyResponse::deserialize(&mut deserializer)?;
+    let response = InboundResponse::deserialize(&mut Deserializer::from(response))?;
     let mut builder = http::Response::builder().status(response.status);
     if let Some(headers) = builder.headers_mut() {
         for (key, value) in &response.headers {
--- spin-http.js
+++ spin-http.js
@@ -1,9 +1,14 @@
 function handleRequest(request) {
+    let dogFact = spin.http.send({
+        method: "GET",
+        uri: "https://some-random-api.ml/facts/dog",
+    })
+
     return {
         status: 200,
         headers: {
             foo: "bar"
         },
-        body: `${spin.config.get("message")}\n`
+        body: `${spin.config.get("message")}\nHere's a dog fact: ${JSON.stringify(dogFact)}\n`
     }
 }
