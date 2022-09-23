19ed5507f99f6cd045ce18fb16dfe932d75059df
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2022-09-22 20:29:03 -0700

    add text and json methods to fetch
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- crates/spin-js-engine/sdk.js
+++ crates/spin-js-engine/sdk.js
@@ -10,6 +10,11 @@ function fetch(uri, options) {
         headers: {
             entries: () => Object.entries(headers)
         },
-        arrayBuffer: () => Promise.resolve(body)
+        arrayBuffer: () => Promise.resolve(body),
+        text: () => Promise.resolve(new TextDecoder().decode(body || new Uint8Array())),
+        json: () => {
+            let text = new TextDecoder().decode(body || new Uint8Array())
+            return Promise.resolve(JSON.parse(text))
+        }
     })
 }
