11fe32e624775697ec090a7c89b834664eb9a4aa
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2022-10-31 16:22:40 -0700

    refactor sdk to typescript
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- crates/spin-js-engine/src/js_sdk/webpack.config.js
+++ crates/spin-js-engine/src/js_sdk/webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    entry: './sdk.js',
+    entry: './sdk.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
         path: path.resolve(__dirname, '../../'),
         filename: 'sdk.js',
