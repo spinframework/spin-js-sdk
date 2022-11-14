9b5192bdd53a3f7582d41e0a11d662c1828b59c6
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2022-11-14 11:55:42 -0800

    Add Url parsing
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- crates/spin-js-engine/src/js_sdk/modules/url.ts
+++ crates/spin-js-engine/src/js_sdk/modules/url.ts
@@ -1,5 +1,7 @@
 /** @internal */
 const Url = require('url-parse')
+/** @internal */
+const _queryString = require('query-string');
 
 interface UrlObject {
     protocol: string
@@ -10,7 +12,7 @@ interface UrlObject {
     host: string
     port: string
     pathname: string
-    query: string
+    search: string
     hash: string
     href: string
     origin: string
@@ -18,8 +20,8 @@ interface UrlObject {
     toString(): string
 }
 
+/** @internal */
 class URL implements UrlObject {
-    /** @internal */
     url: UrlObject
     protocol: string
     slashes: boolean
@@ -29,23 +31,25 @@ class URL implements UrlObject {
     host: string
     port: string
     pathname: string
-    query: string
+    search: string
+    searchParams: URLSearchParams
     hash: string
     href: string
     origin: string
 
     constructor(urlStr: string, base = undefined) {
-        let url:UrlObject = Url(urlStr, base)
+        let url = Url(urlStr, base)
         this.url = url
         this.protocol = url.protocol
-        this.slashes = url.slashes 
+        this.slashes = url.slashes
         this.auth = url.auth
-        this.username =  url.username
-        this.password =  url.password
+        this.username = url.username
+        this.password = url.password
         this.host = url.host
         this.port = url.port
         this.pathname = url.pathname
-        this.query = url.query
+        this.search = url.query
+        this.searchParams = new URLSearchParams(this.search)
         this.hash = url.hash
         this.href = url.origin
         this.origin = url.origin
@@ -61,4 +65,54 @@ class URL implements UrlObject {
 
 }
 
-export { URL }
+/** @internal */
+class URLSearchParams {
+    queryParams: { [key: string]: string | Array<string> } = {}
+
+    constructor(val: string) {
+        this.queryParams = {
+            ..._queryString.parse(val)
+        }
+    }
+    append(key: string, val: string | Array<string>) {
+        this.queryParams[key] = val
+    }
+    delete(key: string) {
+        delete this.queryParams[key]
+    }
+    get(key: string) {
+        let val = this.queryParams[key]
+        if (val) {
+            if (typeof (val) == "object") {
+                return val[0]
+            }
+            return val
+        }
+        return null
+    }
+    getAll(key: string) {
+        let val = this.queryParams[key]
+        if (val) {
+            return val
+        }
+        return null
+    }
+    has(key: string) {
+        return this.queryParams[key] != undefined ? true : false
+    }
+    keys() {
+        return Object.keys(this.queryParams)
+    }
+    set(key: string, val: string | Array<string>) {
+        this.queryParams[key] = val
+    }
+    toString() {
+        return _queryString.stringify(this.queryParams)
+    }
+    values() {
+        return Object.keys(this.queryParams).map(k => this.queryParams[k])
+    }
+}
+
+/** @internal */
+export { URL, URLSearchParams }
