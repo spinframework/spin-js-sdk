e0bcd74819d81a9ed3ba17af2eb86ff289b879a5
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2022-11-18 16:39:29 -0800

    feat/add_redis
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- examples/typescript/hello_world/src/index.ts
+++ examples/typescript/hello_world/src/index.ts
@@ -1,10 +1,10 @@
 import { HandleRequest, HttpResponse } from "spin-sdk-types"
 
 const encoder = new TextEncoder()
+const decoder = new TextDecoder()
 
 export const handleRequest: HandleRequest = async function (request): Promise<HttpResponse> {
 
-    
     return {
         status: 200,
         headers: {"foo": "bar"},
