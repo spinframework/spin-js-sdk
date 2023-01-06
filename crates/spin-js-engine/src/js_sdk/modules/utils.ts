a7173fe6d1122d6f2c9ebd5d0873a6e8040829b5
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2023-01-06 21:18:16 +0530

    add router to the SDK
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- crates/spin-js-engine/src/js_sdk/modules/utils.ts
+++ crates/spin-js-engine/src/js_sdk/modules/utils.ts
@@ -1,21 +1,23 @@
 /** @internal */
 const arrayBufToBuf = require('typedarray-to-buffer')
-
 /** @internal */
-function toBuffer(arg0: ArrayBuffer): Buffer {
-    return arrayBufToBuf(arg0)
-}
+import { router} from "./router"
+import { routerType} from "./router"
 
 /** @internal */
 const utils = {
     toBuffer(arg0: ArrayBuffer): Buffer {
         return arrayBufToBuf(arg0)
-    } 
+    },
+    Router: () => {
+        return router()
+    }
 }
 
 declare global {
     const utils: {
         toBuffer(argo: ArrayBuffer): Buffer
+        Router(): routerType
     }
 }
 
