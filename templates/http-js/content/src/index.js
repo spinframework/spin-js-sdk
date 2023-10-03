3f2f553693359ee94ba8b5c5a743ba794a071470
karthik2804 <karthik.ganeshram@fermyon.com>
2023-10-03 21:43:10 +0530

    update templates
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- templates/http-js/content/src/index.js
+++ templates/http-js/content/src/index.js
@@ -1,10 +1,8 @@
-const encoder = new TextEncoder()
-
 export async function handleRequest(request) {
 
     return {
         status: 200,
         headers: { "foo": "bar" },
-        body: encoder.encode("Hello from JS-SDK").buffer
+        body: "Hello from JS-SDK"
     }
 }
