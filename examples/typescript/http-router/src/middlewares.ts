82382dba139916492571763fbcca376ffcf92ae5
Thorsten Hans <thorsten.hans@gmail.com>
2023-10-18 18:38:59 +0200

    chore: Add HTTP router sample for TypeScript
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

---
+++ examples/typescript/http-router/src/middlewares.ts
@@ -0,0 +1,7 @@
+declare type RequestLike = {
+    [key: string]: any
+}
+
+export function withCorrelationId(req: RequestLike): void {
+    req.correlationId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
