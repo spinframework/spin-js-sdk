e246c64d9492c977a2454a1387e60d9c62bd8648
Radu Matei <radu@fermyon.com>
2024-04-24 12:12:29 +0200

    Update template with the right location for the SDK
    Signed-off-by: Radu Matei <radu@fermyon.com>

--- templates/http-ts/content/src/spin.ts
+++ templates/http-ts/content/src/spin.ts
@@ -1,7 +1,7 @@
 import { HttpHandler, HttpRequest, ResponseBuilder } from "@fermyon/spin-sdk";
 import { handler } from ".";
 
-class App extends HandlerHandler {
+class App extends HttpHandler {
     handleRequest(req: HttpRequest, res: ResponseBuilder): Promise<void> {
         return handler(req, res)
     }
