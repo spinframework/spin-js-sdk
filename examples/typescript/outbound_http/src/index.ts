0bdf0bcfdedfd8ee4a0bd6b28281df0d5688a551
Sohan <1119120+sohanmaheshwar@users.noreply.github.com>
2024-04-26 11:34:36 +0530

    Update index.ts
    Hey @vdice 
    
    Was building this for a conf demo which is happening in a couple of hours and the sample showed an error. I quickly made this change and it works, but it might not be the 'right' way to do it. 
    just a heads-up
    
    Signed-off-by: Sohan <1119120+sohanmaheshwar@users.noreply.github.com>

--- examples/typescript/outbound_http/src/index.ts
+++ examples/typescript/outbound_http/src/index.ts
@@ -6,13 +6,13 @@ const decoder = new TextDecoder()
 export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
 
     const physicsFact = await fetch("https://random-data-api.fermyon.app/physics/json")
-
+  
     const physicsFactBody = await physicsFact.text()
 
-    const env = JSON.stringify(process.env)
+    const physicsFactObject = JSON.parse(physicsFactBody)
 
-    const body = `${Config.get("message")}\nenv: ${env}\nHere's a physics fact: ${physicsFactBody}\n`
-
+    const body = `Here's a physics fact: ${JSON.parse(physicsFactBody).fact}\n`
+    
     return {
         status: 200,
         headers: { "foo": "bar" },
