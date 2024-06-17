86eff5bac64da7b6d96c7f9d0f01003a9d4f7d7c
karthik2804 <karthik.ganeshram@fermyon.com>
2024-06-17 15:02:53 +0200

    update tests to use local lib
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- test/test-app/src/test.ts
+++ test/test-app/src/test.ts
@@ -1,14 +1,14 @@
-import { HttpRequest, ResponseBuilder } from "@fermyon/spin-sdk";
+import { ResponseBuilder } from "@fermyon/spin-sdk";
 
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
 
-function health(req: HttpRequest, res: ResponseBuilder) {
+function health(req: Request, res: ResponseBuilder) {
     res.status(200)
     res.send("Server is healthy")
 }
 
-async function testFunctionality(req: HttpRequest, res: ResponseBuilder) {
+async function testFunctionality(req: Request, res: ResponseBuilder) {
     //TODO: Add tests once fetch is available
     res.status(200)
     res.send("test completed successfully")
