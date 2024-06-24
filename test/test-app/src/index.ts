2f70d29cf75097e8c14303dd8db10c9f57ae3691
karthik2804 <karthik.ganeshram@fermyon.com>
2024-06-24 11:10:08 +0200

    add some more tests for SDK
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- test/test-app/src/index.ts
+++ test/test-app/src/index.ts
@@ -1,10 +1,15 @@
 import { ResponseBuilder, Router } from "@fermyon/spin-sdk";
-import { testFunctionality, health } from "./test";
+import { testFunctionality, health, statusTest, headersTest, outboundHttp, kvTest } from "./test";
 
 let router = Router()
 
 router.get("/", async (_, req, res) => { await testFunctionality(req, res) })
+router.get("/statusTest", async (_, req, res) => { await statusTest(req, res) })
+router.get("/headersTest", async (_, req, res) => { await headersTest(req, res) })
+router.get("/outboundHttp", async (_, req, res) => { await outboundHttp(req, res) })
+router.get("/kvTest", async (_, req, res) => { await kvTest(req, res) })
 router.get("/health", async (_, req, res) => { await health(req, res) })
+router.all("*", async (_, req, res) => { res.status(404); res.send(); })
 
 export async function handler(req: Request, res: ResponseBuilder) {
     await router.handleRequest(req, res)
