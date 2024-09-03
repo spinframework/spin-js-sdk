69156981d789c2f7225fefc20b88a3140e95816f
karthik2804 <karthik.ganeshram@fermyon.com>
2024-09-03 10:08:36 +0200

    bump spin_sdk dep
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- templates/redis-ts/content/src/index.ts
+++ templates/redis-ts/content/src/index.ts
@@ -1,8 +1,10 @@
 import { RedisHandler } from "@fermyon/spin-sdk";
 
+const decoder = new TextDecoder();
+
 export class MyRedisHandler extends RedisHandler {
     async handleMessage(msg: Uint8Array): Promise<void> {
-        console.log("Received message:", msg);
+        console.log("Received message:", decoder.decode(msg));
     }
 }
 
