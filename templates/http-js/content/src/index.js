64bde83dac745872aea490cc2f5d341de4d9851d
karthik2804 <karthik.ganeshram@fermyon.com>
2025-01-21 08:21:43 +0100

    address review comments
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- templates/http-js/content/src/index.js
+++ templates/http-js/content/src/index.js
@@ -1,13 +1,14 @@
 import { ResponseBuilder, Router } from "@fermyon/spin-sdk";
 
 let router = Router();
-router.get("*", (_, req, res) => { handleDefaultRoute(req, res) })
-router.all("*", (_, req, res) => { notFound(req, res) })
+// Modify this route or add additional ones to implement the component's API:
+router.get("/hello/:name", (metadata, req, res) => { handleHelloRoute(req, res, metadata.params.name) });
+// Default route that will be called for any routes not handled above:
+router.all("*", (_, req, res) => { notFound(req, res) });
 
-async function handleDefaultRoute(req, res) {
-    res.send("hello universe");
+async function handleHelloRoute(req, res, name) {
+    res.send(`hello ${name}`);
 }
-
 async function notFound(req, res) {
     res.status(404);
     res.send("not found");
--- templates/http-ts/content/src/index.ts
+++ templates/http-ts/content/src/index.ts
@@ -1,13 +1,14 @@
 import { ResponseBuilder, Router } from "@fermyon/spin-sdk";
 
 let router = Router();
-router.get("*", (_, req, res) => { handleDefaultRoute(req, res) })
-router.all("*", (_, req, res) => { notFound(req, res) })
+// Modify this route or add additional ones to implement the component's API:
+router.get("/hello/:name", (metadata, req, res) => { handleHelloRoute(req, res, metadata.params.name) });
+// Default route that will be called for any routes not handled above:
+router.all("*", (_, req, res) => { notFound(req, res) });
 
-async function handleDefaultRoute(req: Request, res: ResponseBuilder) {
-    res.send("hello universe");
+async function handleHelloRoute(req: Request, res: ResponseBuilder, name: string) {
+    res.send(`hello ${name}`);
 }
-
 async function notFound(req: Request, res: ResponseBuilder) {
     res.status(404);
     res.send("not found");
