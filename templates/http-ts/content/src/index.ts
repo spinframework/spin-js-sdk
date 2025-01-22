62969b9f1b05d5f0509d69ddbb493a7ed186276c
karthik2804 <karthik.ganeshram@fermyon.com>
2025-01-22 23:58:22 +0100

    unbreak spin CI test
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- templates/http-ts/content/src/index.ts
+++ templates/http-ts/content/src/index.ts
@@ -1,14 +1,21 @@
 import { ResponseBuilder, Router } from "@fermyon/spin-sdk";
 
 let router = Router();
+// Route that will be called for the default route:
+router.get("/", (_, req, res) => { handleDefaultRoute(req, res) });
 // Modify this route or add additional ones to implement the component's API:
 router.get("/hello/:name", (metadata, req, res) => { handleHelloRoute(req, res, metadata.params.name) });
 // Default route that will be called for any routes not handled above:
 router.all("*", (_, req, res) => { notFound(req, res) });
 
+async function handleDefaultRoute(req: Request, res: ResponseBuilder) {
+    res.send("hello universe");
+}
+
 async function handleHelloRoute(req: Request, res: ResponseBuilder, name: string) {
     res.send(`hello ${name}`);
 }
+
 async function notFound(req: Request, res: ResponseBuilder) {
     res.status(404);
     res.send("not found");
