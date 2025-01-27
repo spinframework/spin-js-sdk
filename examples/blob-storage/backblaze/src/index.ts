60cc082c01d1738bbc6cb7848d13666bb0f67de9
karthik2804 <karthik.ganeshram@fermyon.com>
2025-01-27 15:04:29 +0100

    update blob-storage
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- examples/blob-storage/backblaze/src/index.ts
+++ examples/blob-storage/backblaze/src/index.ts
@@ -1,36 +1,46 @@
+// https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
 import {
-  CreateBucketCommand,
-  PutObjectCommand,
-  S3Client,
+    CreateBucketCommand,
+    PutObjectCommand,
+    S3Client,
 } from '@aws-sdk/client-s3';
-import { ResponseBuilder } from '@fermyon/spin-sdk';
 import { v4 as uuid } from 'uuid';
 
 const s3 = new S3Client({
-  endpoint: '<Backblaze b2 endpoint>',
-  region: '<>',
-  credentials: {
-    accessKeyId: '<>',
-    secretAccessKey: '<>',
-  },
+    endpoint: '<Backblaze b2 endpoint>',
+    region: '<>',
+    credentials: {
+        accessKeyId: '<>',
+        secretAccessKey: '<>',
+    },
 });
+let router = AutoRouter();
 
-export async function handler(_req: Request, res: ResponseBuilder) {
-  try {
-    let bucketName = 'spin-sdk-bucket-' + uuid();
-    let keyName = 'hello_world.txt';
-    await s3.send(new CreateBucketCommand({ Bucket: bucketName }));
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+    .get("/", async () => {
+        try {
+            let bucketName = 'spin-sdk-bucket-' + uuid();
+            let keyName = 'hello_world.txt';
+            await s3.send(new CreateBucketCommand({ Bucket: bucketName }));
 
-    await s3.send(
-      new PutObjectCommand({
-        Bucket: bucketName,
-        Key: keyName,
-        Body: 'Hello World!',
-      }),
-    );
-    res.send(`Successfully uploaded data to ${bucketName}/${keyName}`);
-  } catch (e: any) {
-    res.status(500);
-    res.send(`error: ${e}`);
-  }
-}
+            await s3.send(
+                new PutObjectCommand({
+                    Bucket: bucketName,
+                    Key: keyName,
+                    Body: 'Hello World!',
+                }),
+            );
+            return new Response(`Successfully uploaded data to ${bucketName}/${keyName}`);
+        } catch (e: any) {
+            return new Response(`error: ${e}`, { status: 500 });
+        }
+    })
+
+//@ts-ignore
+addEventListener('fetch', async (event: FetchEvent) => {
+    event.respondWith(router.fetch(event.request));
+});
