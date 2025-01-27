b13db01a7cdc34b09ca1f95fa03b9ebb52e24fbe
karthik2804 <karthik.ganeshram@fermyon.com>
2025-01-27 14:02:14 +0100

    update upstash examples
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- examples/upstash/qstash/src/index.ts
+++ examples/upstash/qstash/src/index.ts
@@ -1,14 +1,25 @@
-import { ResponseBuilder } from '@fermyon/spin-sdk';
+// https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
 import { Client } from '@upstash/qstash';
 
 const client = new Client({ token: '<upstash token>' });
+let router = AutoRouter();
 
-export async function handler(_req: Request, res: ResponseBuilder) {
-  const resp = await client.publishJSON({
-    url: '<>',
-    body: { hello: 'world' },
-    delay: 2,
-  });
-  console.log(resp);
-  res.send('hello universe');
-}
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+    .get("/", async () => {
+        let resp = await client.publishJSON({
+            url: '<>',
+            body: { hello: 'world' },
+            delay: 2,
+        });
+
+        return new Response(JSON.stringify(resp), { status: 200 });
+    })
+
+//@ts-ignore
+addEventListener('fetch', async (event: FetchEvent) => {
+    event.respondWith(router.fetch(event.request));
+});
--- examples/upstash/vector/src/index.ts
+++ examples/upstash/vector/src/index.ts
@@ -1,32 +1,43 @@
-import { ResponseBuilder } from '@fermyon/spin-sdk';
+// https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
 import { Index } from '@upstash/vector';
 
 const index = new Index({
-  url: '<upstash URL>',
-  token: '<Upstash token>',
+    url: '<upstash URL>',
+    token: '<Upstash token>',
 });
 
-export async function handler(_req: Request, res: ResponseBuilder) {
-  try {
-    await index.upsert({
-      id: '1',
-      vector: [0.6, 0.8],
-      metadata: { field: 'value' },
-    });
-    await index.upsert({
-      id: '2',
-      vector: [0.6, 0.6],
-      metadata: { field: 'value' },
-    });
+let router = AutoRouter();
 
-    let data = await index.query({
-      vector: [0.6, 0.7],
-      topK: 3,
-      includeMetadata: true,
-    });
-    res.send(JSON.stringify(data, null, 2));
-  } catch (e: any) {
-    res.status(500);
-    res.send(`error: ${e}`);
-  }
-}
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+    .get("/", async () => {
+        try {
+            await index.upsert({
+                id: '1',
+                vector: [0.6, 0.8],
+                metadata: { field: 'value' },
+            });
+            await index.upsert({
+                id: '2',
+                vector: [0.6, 0.6],
+                metadata: { field: 'value' },
+            });
+
+            let data = await index.query({
+                vector: [0.6, 0.7],
+                topK: 3,
+                includeMetadata: true,
+            });
+            return new Response(JSON.stringify(data, null, 2), { status: 200 });
+        } catch (e: any) {
+            return new Response(`error: ${e}`, { status: 500 });
+        }
+    })
+
+//@ts-ignore
+addEventListener('fetch', async (event: FetchEvent) => {
+    event.respondWith(router.fetch(event.request));
+});
