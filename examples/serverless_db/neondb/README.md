4ae5ae20630b8957532f366bdbfaa50e9d79647f
karthik2804 <karthik.ganeshram@fermyon.com>
2025-01-27 15:49:12 +0100

    update serverlessdb examples
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- examples/serverless_db/neondb/README.md
+++ examples/serverless_db/neondb/README.md
@@ -2,13 +2,6 @@
 
 This example demonstrates how to connect to Neon Database using the Neon Serverless SDK, execute a SQL query, and handle responses using the Spin SDK.
 
-## Install Dependencies
-Install the necessary npm packages:
-
-```bash
-npm install
-```
-
 ## Setup the Example
 
 1. **Setup Neon Database**
--- examples/serverless_db/neondb/src/index.ts
+++ examples/serverless_db/neondb/src/index.ts
@@ -1,13 +1,24 @@
-import { ResponseBuilder } from '@fermyon/spin-sdk';
+// https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
 import { neon } from '@neondatabase/serverless';
 
-export async function handler(_req: Request, res: ResponseBuilder) {
-  try {
-    const sql = neon('<neon-database-endpoint>');
-    const posts = await sql('SELECT * FROM posts');
-    res.send(JSON.stringify(posts, null, 2));
-  } catch (e: any) {
-    res.status(500);
-    res.send(`error: ${e}`);
-  }
-}
+let router = AutoRouter();
+
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+    .get("/", async () => {
+        try {
+            const sql = neon('<neon-database-endpoint>');
+            const posts = await sql('SELECT * FROM posts');
+            return new Response(JSON.stringify(posts, null, 2), { headers: { 'Content-Type': 'application/json' } });
+        } catch (e: any) {
+            return new Response(`error: ${e}`, { status: 500 });
+        }
+    })
+
+//@ts-ignore
+addEventListener('fetch', async (event: FetchEvent) => {
+    event.respondWith(router.fetch(event.request));
+});
