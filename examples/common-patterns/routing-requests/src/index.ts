30fe6954175bc8fec88d3254f19c191b741ac393
Thorsten Hans <thorsten.hans@fermyon.com>
2025-02-11 09:58:10 +0100

    chore: updated router-sample
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- examples/common-patterns/routing-requests/src/index.ts
+++ examples/common-patterns/routing-requests/src/index.ts
@@ -7,10 +7,51 @@ let router = AutoRouter();
 // Any route that does not return will be treated as a middleware
 // Any unmatched route will return a 404
 router
-    .get("/", () => new Response("hello universe"))
-    .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+  .get('/', () => new Response('Hello, Fermyon'))
+  .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+  .get('/bye/:name', ({ name }) => sayGoodBye(name))
+  .get('/reverse-header-value', req => reverseHeaderValue(req))
+  .post('/items', async (req) => storeItem(await req.arrayBuffer()));
+
 
 //@ts-ignore
 addEventListener('fetch', async (event: FetchEvent) => {
-    event.respondWith(router.fetch(event.request));
+  event.respondWith(router.fetch(event.request));
 });
+
+const sayGoodBye = (name: string): Response => {
+  const payload = {
+    message: `Goodbye, ${name}`
+  };
+  return new Response(JSON.stringify(payload), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+}
+
+const reverseHeaderValue = (req: Request): Response => {
+  const headerValue = req.headers.get('x-spin-demo');
+  if (!headerValue) {
+    return new Response('Bad Request', { status: 400 });
+  }
+  return new Response(undefined, {
+    status: 200, headers: {
+      'x-spin-demo': headerValue.split('').reverse().join('')
+    }
+  });
+}
+
+const storeItem = (requestBody: ArrayBuffer): Response => {
+  const decoder = new TextDecoder();
+  let payload;
+  try {
+    payload = JSON.parse(decoder.decode(requestBody));
+  }
+  catch (error) {
+    return new Response('Bad Request', { status: 400 });
+  }
+  console.log(`Storing item ${JSON.stringify(payload)}`);
+  return new Response('Item stored', { status: 201 });
+};
