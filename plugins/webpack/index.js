32942c41f6d9cc2daa988c160189bb0ae9316e78
karthik2804 <karthik.ganeshram@fermyon.com>
2025-02-24 14:32:18 +0100

    fix webpack externals
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- plugins/webpack/index.js
+++ plugins/webpack/index.js
@@ -1,28 +1,26 @@
 class SpinSdkPlugin {
     constructor() {
-        this.externals = {
-            "wasi:http/types@0.2.0": "wasi:http/types@0.2.0",
-            "wasi:cli/environment@0.2.0": "wasi:cli/environment@0.2.0",
-            "wasi:filesystem/preopens@0.2.0": "wasi:filesystem/preopens@0.2.0",
-            "fermyon:spin/llm@2.0.0": "fermyon:spin/llm@2.0.0",
-            "fermyon:spin/variables@2.0.0": "fermyon:spin/variables@2.0.0",
-            "fermyon:spin/redis@2.0.0": "fermyon:spin/redis@2.0.0",
-            "fermyon:spin/key-value@2.0.0": "fermyon:spin/key-value@2.0.0",
-            "fermyon:spin/sqlite@2.0.0": "fermyon:spin/sqlite@2.0.0",
-            "fermyon:spin/postgres@2.0.0": "fermyon:spin/postgres@2.0.0",
-            "fermyon:spin/mysql@2.0.0": "fermyon:spin/mysql@2.0.0",
-            "fermyon:spin/mqtt@2.0.0": "fermyon:spin/mqtt@2.0.0",
-            "wasi:config/store@0.2.0-draft-2024-09-27": "wasi:config/store@0.2.0-draft-2024-09-27",
-            "spin:postgres/postgres@3.0.0": "spin:postgres/postgres@3.0.0",
-        };
+        this.externals = [
+            /^wasi:.*/,
+            /^fermyon:.*/,
+            /^spin:.*/,
+        ];
     }
 
     apply(compiler) {
         // If externals are already defined, merge them with new externals
-        if (compiler.options.externals && typeof compiler.options.externals === 'object') {
-            this.externals = Object.assign({}, compiler.options.externals, this.externals);
+        const userExternals = compiler.options.externals;
+
+        if (Array.isArray(userExternals)) {
+            // Append to the existing array
+            compiler.options.externals = [...userExternals, ...this.externals];
+        } else if (typeof userExternals === 'object') {
+            // Wrap in an array and add the object as-is
+            compiler.options.externals = [userExternals, ...this.externals];
+        } else {
+            // Default: Just use our externals
+            compiler.options.externals = [...this.externals];
         }
-        compiler.options.externals = this.externals;
     }
 }
 
